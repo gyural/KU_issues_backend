31c00cf93c570e102641ce0b41acba3bb4cefce0
dgsim <dgsim126@gmail.com>
2024-05-25 16:18:53 +0900

    third commit

--- routers/postRoute.js
+++ routers/postRoute.js
@@ -6,7 +6,7 @@ const { PostModel } = require("../models/index"); // 수정
 
 /**
  * 모든 게시판 내용 가져오기
- * GET /api/posts/
+ * GET /api/posts
  */
 router.get("/", asyncHandler(async(req, res)=>{
     const data= await Post.findAll(); // DB에서 모든 데이터를 다 가져옴
@@ -37,13 +37,26 @@ router.post("/create", asyncHandler(async(req, res)=>{
         });
         console.log("데이터베이스에 값 삽입 완료");
 
-
         res.redirect("/api/posts/");
     }catch(error){
         console.log("게시물 생성 중 에러 발생", error);
         res.status(500).send("게시물 생성 중 에러가 발생");
     }
 }));
+
+/**
+ * 게시글 내용 확인
+ * GET api/posts/{post_id}
+ */
+router.get("/:post_id", asyncHandler(async (req, res) => {
+    const post = await Post.findByPk(req.params.post_id);
+    if (post) {
+        res.render("showPostDetail", { post: post });
+    } else {
+        res.status(404).send("Post not found");
+    }
+}));
+
 
 
 
