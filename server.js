c9ce78f00dbb47806c89499290f5b367e7dc1aff
Shihyun Han <devowl99@gmail.com>
2024-05-26 02:00:05 +0900

    third commit

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 
 const { conn } = require('./db');
 const { sequelize } = require("./models/index")
+const { User } = require('./models/user/userModel'); // 추가
 
 const app = express();
 const port = 3000;
@@ -10,15 +11,15 @@ const port = 3000;
 sequelize
 .sync({force: false})
 .then(()=>{
-  console.log('데이터 베이스 연결 성공')
+    console.log('데이터베이스 연결 성공')
 }).catch(err=>{
-  console.log(err)
+    console.log(err)
 })
 
 app.use(express.json())
-app.use("/api", require("./routers/loginRoute")) // 수정 - loginRoute로 수정
-app.use('/api/survey', require("./routers/surveyRoute"))
 
+// app.use("/api", require("./routers/loginRoute")) // 1. 수정 - loginRoute로 수정 2. 전체 주석
+app.use('/api/survey', require("./routers/surveyRoute"))
 
 app.get('/', async (req, res) => {
     try {
@@ -30,6 +31,27 @@ app.get('/', async (req, res) => {
         res.status(500).send(`Database connection failed: ${err.message}`);
     }
 })
+
+// 추가 - /profile
+app.get('/profile', async (req, res) => {
+    try {
+        const user = await User.findOne({
+            where: { id: req.user.id },
+            attributes: ['id', 'name', 'nickname', 'grade']
+        });
+  
+        // 사용자가 존재하지 않는 경우
+        if (!user) {
+            return res.status(404).send('User not found');
+        }
+  
+        // 사용자가 존재하는 경우
+        res.json(user);
+    } catch (err) {
+        console.error('Error fetching profile:', err);
+        res.status(500).send('Error 500');
+    }
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
