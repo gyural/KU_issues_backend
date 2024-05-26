606b0fee46e40c96e467c0fc29e32a3259d27f1f
Shihyun Han <devowl99@gmail.com>
2024-05-26 17:28:10 +0900

    5th commit

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,15 +1,31 @@
 const asyncHandler = require("express-async-handler")
-const Profile = require("../models/user/userModel");
+const User = require('../models/user/userModel');
 
 // GET /api/profile/:id
-const getProfile = asyncHandler(async (req, res) => { // findById
-    res.status(200).send(`GET Profile : ${req.params.id}`)
+const getProfile = asyncHandler(async (req, res) => {
+    const id = req.params.id;
+    const user = await User.findByPk(id);
+    if (!user) {
+        res.status(404).send('404 User not found');
+    } else {
+        res.json(user);
+    }
 });
 
 // PUT /api/profile/:id
-const updateProfile = asyncHandler(async (req, res) => { // findByIdAndUpdate
-    console.log(req.body)
-    res.status(200).send(`PUT Profile : ${req.params.id}`)
+const updateProfile = asyncHandler(async (req, res) => {
+    const id = req.params.id;
+    const { name, nickname, grade } = req.body;
+    const user = await User.findByPk(id);
+    if (!user) {
+        res.status(404).send('User not found');
+    } else {
+        user.name = name;
+        user.nickname = nickname;
+        user.grade = grade;
+        await user.save();
+        res.send('User updated successfully');
+    }
 });
 
 module.exports = { getProfile, updateProfile };
