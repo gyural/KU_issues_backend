673f450ea1900520cf576b90aa937cce0abf880d
dgsim <dgsim126@gmail.com>
2024-05-31 02:03:03 +0900

    server.js

--- server.js
+++ server.js
@@ -2,17 +2,27 @@ require('dotenv').config();
 const express = require('express');
 
 const { sequelize } = require("./models/index");
+const surveyRouter = require("./routers/surveyRoute");
+const postRouter= require("./routers/postRoute");  
 
 const app = express();
 const port = 3000;
 
 sequelize
-.sync({force: true})
+.sync({force: false})
 .then(()=>{
     console.log('데이터베이스 연결 성공')
 }).catch(err=>{
     console.log(err)
 })
+
+// EJS 뷰 엔진 설정
+app.set('view engine', 'ejs');
+
+// 미들웨어 설정
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // URL-encoded 데이터 파싱
+
 
 app.use(express.json())
 app.use('/api', require('./routers/profileRoute'));
@@ -20,7 +30,7 @@ app.use("/api", require("./routers/loginRoute"))
 app.use('/api/survey', require("./routers/surveyRoute"))
 app.use('/api/comments', require('./routers/commentRoute'));
 app.use('/api/votes', require('./routers/voteRoute'));
-app.use('/api/posts', require)
+app.use('/api/posts', postRouter);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
