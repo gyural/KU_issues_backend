0032905129bbb6c154a07008e944567d67d0f918
jangse0 <151913785+jangse0@users.noreply.github.com>
2024-06-02 03:02:58 +0900

    Update commentRoute.js

--- routers/commentRoute.js
+++ routers/commentRoute.js
@@ -2,14 +2,15 @@ const express = require("express");
 const router = express.Router();
 const asyncHandler = require("express-async-handler");
 const { getCommentsByPost, createComment, deleteComment } = require("../controllers/commentController");
+const verifyToken = require('../middleware/token');
 
 // 특정 게시글의 모든 댓글 조회
 router.get("/:post_id", asyncHandler(getCommentsByPost));
 
 // 댓글 생성
-router.post("/:post_id/create", asyncHandler(createComment));
+router.post("/:post_id/create",verifyToken, asyncHandler(createComment));
 
 // 댓글 삭제
-router.delete("/:comment_id", asyncHandler(deleteComment));
+router.delete("/:comment_id",verifyToken, asyncHandler(deleteComment));
 
 module.exports = router;
